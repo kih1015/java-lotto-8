da0505dcc27fab3c75713bea5f2ca8fbded6cf0e
강인화 <rkddlsghk1998@gmail.com>
2025-11-02 23:46:58 +0900

    feat: 로또 구매금액 입력 기능 구현
    입력 메시지 출력
    로또 구입 금액 입력
    Money 클래스 구현

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,21 @@
+package lotto;
+
+public record Money(Integer amount) {
+
+    public Money {
+        validateAmount(amount);
+    }
+
+    public int getPurchases() {
+        return amount / 1000;
+    }
+
+    private void validateAmount(Integer amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 구매금액은 0원보다 많아야 합니다.");
+        }
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매금액은 1000원 단위만 가능합니다.");
+        }
+    }
+}
