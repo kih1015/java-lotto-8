eac2c3159db24fcb6c18aa3e4a176d53eb53bd3e
Kevin(강인화) <rkddlsghk1998@jinhakapply.com>
2025-11-03 16:41:51 +0900

    feat: 문자열 숫자 변환 시 예외 구현

--- src/main/java/lotto/ConsoleReader.java
+++ src/main/java/lotto/ConsoleReader.java
@@ -9,14 +9,22 @@ public class ConsoleReader {
     public Money inputMoney() {
         System.out.println("구매금액을 입력해 주세요.");
         String purchasePrice = Console.readLine();
-        return new Money(Integer.parseInt(purchasePrice));
+        return new Money(parseInt(purchasePrice));
     }
 
     public WinningLotto inputWinningLotto() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        Lotto winningNumber = new Lotto(Arrays.stream(Console.readLine().split(",")).map(Integer::parseInt).toList());
+        Lotto winningNumber = new Lotto(Arrays.stream(Console.readLine().split(",")).map(this::parseInt).toList());
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
-        return new WinningLotto(winningNumber, Integer.parseInt(bonusNumber));
+        return new WinningLotto(winningNumber, parseInt(bonusNumber));
+    }
+
+    private Integer parseInt(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
     }
 }
