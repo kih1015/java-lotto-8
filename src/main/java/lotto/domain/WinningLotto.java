19d9fe5c5854590948a90bc791d620b26fb6d997
Kevin(강인화) <rkddlsghk1998@jinhakapply.com>
2025-11-03 17:54:07 +0900

    feat: 보너스 번호가 당첨 번호와 중복될 경우 예외 처리

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -8,8 +8,15 @@ public class WinningLotto {
     private final int bonusNumber;
 
     public WinningLotto(Lotto winningNumber, int bonusNumber) {
+        validateBonusNumber(winningNumber, bonusNumber);
         this.winningNumber = winningNumber;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Lotto winningNumber, int bonusNumber) {
+        if (winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
     }
 
     public Optional<Rank> rank(Lotto lotto) {
