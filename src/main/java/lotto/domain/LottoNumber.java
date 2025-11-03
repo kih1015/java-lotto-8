a6b2f5f5e8dcfcf3bdcc79f4643473f1f84b5b35
강인화 <rkddlsghk1998@gmail.com>
2025-11-03 21:10:14 +0900

    feat: 로또 숫자 범위 제한

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,4 +2,13 @@ package lotto.domain;
 
 public record LottoNumber(Integer number) {
 
+    public LottoNumber {
+        validate(number);
+    }
+
+    private void validate(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 숫자 범위는 1~45 입니다.");
+        }
+    }
 }
